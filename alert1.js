ed61be301c65a0c2a4bcfe1fe3bb4056ce6f30f6
ireneshevchenko <47928401+ireneshevchenko@users.noreply.github.com>
2019-03-22 18:59:35 +0200

    Add files via upload

---
+++ alert1.js
@@ -0,0 +1,10 @@
+function displayEvenNumbers (x, y) {
+for (var i = x; i<= y; i++) {
+if (i % 2  == 0) { 
+	console.log (i);
+}
+}
+}
+displayEvenNumbers (1, 4);
+displayEvenNumbers (3, 9);
+displayEvenNumbers (1, 11);
